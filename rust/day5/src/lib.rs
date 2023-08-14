357c9a3d447db31685249d05d48c70b33cbcda0e
khsaad04 <khsaad0404@gmail.com>
2023-08-14 20:26:22 +0600

    update

---
+++ rust/day5/src/lib.rs
@@ -0,0 +1,35 @@
+pub fn process_part1(input: &str) -> String {
+    "".to_string()
+}
+
+pub fn process_part2(input: &str) -> String {
+    "".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+";
+
+    #[test]
+    fn part1() {
+        let result = process_part1(INPUT);
+        assert_eq!(result, "CMZ");
+    }
+
+    #[test]
+    fn part2() {
+        let result = process_part2(INPUT);
+        assert_eq!(result, "");
+    }
+}
