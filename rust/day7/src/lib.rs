5b64a335491878ae920842a2beeb61b252283774
khsaad04 <khsaad0404@gmail.com>
2023-08-15 20:18:01 +0600

    update

--- rust/day7/src/lib.rs
+++ rust/day7/src/lib.rs
@@ -1,5 +1,11 @@
+fn parse(input: &str) -> Vec<&str> {
+    let vec = input.split("$ ").collect();
+    vec
+}
+
 pub fn process_part1(input: &str) -> String {
     let result: usize = 0;
+    let vec = parse(&input);
     result.to_string()
 }
 
@@ -12,12 +18,35 @@ pub fn process_part2(input: &str) -> String {
 mod tests {
     use super::*;
 
-    const INPUT: &str = "";
+    const INPUT: &str = "$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
+";
 
     #[test]
     fn part1() {
         let result = process_part1(INPUT);
-        assert_eq!(result, "");
+        assert_eq!(result, "95437");
     }
 
     #[test]
