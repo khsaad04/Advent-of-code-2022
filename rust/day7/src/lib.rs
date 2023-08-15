c36374dd6db209f7f40f18aefce70de7dde6aaad
khsaad04 <khsaad0404@gmail.com>
2023-08-15 12:52:10 +0600

    update

---
+++ rust/day7/src/lib.rs
@@ -0,0 +1,28 @@
+pub fn process_part1(input: &str) -> String {
+    let result: usize = 0;
+    result.to_string()
+}
+
+pub fn process_part2(input: &str) -> String {
+    let result: usize = 0;
+    result.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "";
+
+    #[test]
+    fn part1() {
+        let result = process_part1(INPUT);
+        assert_eq!(result, "");
+    }
+
+    #[test]
+    fn part2() {
+        let result = process_part2(INPUT);
+        assert_eq!(result, "");
+    }
+}
