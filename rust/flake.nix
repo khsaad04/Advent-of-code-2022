540bee9be4b31d969f857e2ca5d4e3049350b5fa
khsaad04 <khsaad0404@gmail.com>
2024-03-19 22:23:58 +0600

    fix devshell

--- rust/flake.nix
+++ rust/flake.nix
@@ -6,31 +6,35 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = {
-    nixpkgs,
-    fenix,
-    ...
-  }: let
-    system = "x86_64-linux";
-    toolchain = "stable";
-    overlays = [fenix.overlays.default];
-    pkgs = import nixpkgs {
-      inherit system overlays;
-    };
-    rustPkg = pkgs.fenix."${toolchain}".withComponents [
-      "cargo"
-      "clippy"
-      "rust-src"
-      "rustc"
-      "rustfmt"
-    ];
-  in {
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      nativeBuildInputs = with pkgs; [
-        gdb
-        rustPkg
-        rust-analyzer-nightly
+  outputs =
+    { nixpkgs
+    , fenix
+    , ...
+    }:
+    let
+      system = "x86_64-linux";
+      toolchain = "stable";
+      overlays = [ fenix.overlays.default ];
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
+      rustPkg = pkgs.fenix."${toolchain}".withComponents [
+        "cargo"
+        "clippy"
+        "rust-src"
+        "rustc"
+        "rustfmt"
       ];
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs;[
+          nil
+          gdb
+          rustPkg
+          rust-analyzer-nightly
+        ];
+      };
+      formatter.${system} = pkgs.nixpkgs-fmt;
     };
-  };
 }
