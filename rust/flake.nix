8d0b8f1826c797cf944901957fcc2051e9c1eade
khsaad04 <khsaad0404@gmail.com>
2024-04-15 20:30:15 +0600

    Update flake for rust

--- rust/flake.nix
+++ rust/flake.nix
@@ -1,38 +1,24 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
   outputs =
     { nixpkgs
-    , fenix
     , ...
     }:
     let
       system = "x86_64-linux";
-      toolchain = "stable";
-      overlays = [ fenix.overlays.default ];
-      pkgs = import nixpkgs {
-        inherit system overlays;
-      };
-      rustPkg = pkgs.fenix."${toolchain}".withComponents [
-        "cargo"
-        "clippy"
-        "rust-src"
-        "rustc"
-        "rustfmt"
-      ];
+      pkgs = nixpkgs.legacyPackages.${system};
     in
     {
       devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs;[
-          nil
-          gdb
-          rustPkg
-          rust-analyzer-nightly
+        packages = [
+          pkgs.nil
+          pkgs.cargo
+          pkgs.clippy
+          pkgs.rustc
+          pkgs.rustfmt
+          pkgs.rust-analyzer
         ];
       };
       formatter.${system} = pkgs.nixpkgs-fmt;
